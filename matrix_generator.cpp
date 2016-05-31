9a5d263122304aee41ec2a37eacce3c1e8449cab
Sachin Sanil <yx11ufeg@stud.informatik.uni-erlangen.de>
2016-05-31 16:12:11 +0200

    31-05-2016 update

--- matrix_generator.cpp
+++ matrix_generator.cpp
@@ -17,6 +17,7 @@ double k_func(double x, double y)
 
 void Matrix_Generator::local_stiffness_matrix(face f)
 {
+    static int face_num =0;
     ELEMENTS::Triangle my_element;
     corners.resize(6,0);
 
@@ -24,9 +25,13 @@ void Matrix_Generator::local_stiffness_matrix(face f)
     corners[2] = i.V[f.v2].x; corners[3] = i.V[f.v2].y;
     corners[4] = i.V[f.v3].x; corners[5] = i.V[f.v3].y;
 
-    /*cout << f.v1 << '\t' << f.v2 << '\t' << f.v3 << endl;
+
+    cout<< "Vertices " << endl;
+    cout << face_num << '\t'<< f.v1 << '\t' << f.v2 << '\t' << f.v3 << endl;
+
+    cout<< "vertix co-ordinates" <<endl;
     for(int i=0 ; i<5 ;i+=2)
-        cout<< corners[i] << '\t' << corners[i+1] << endl;*/
+        cout<< corners[i] << '\t' << corners[i+1] << endl;
 
    my_element(corners);
 
@@ -35,37 +40,33 @@ void Matrix_Generator::local_stiffness_matrix(face f)
 
    Helmholtz_Stencil = my_element.integrate(func<double>(k_func) *v_() * w_());
 
-   my_local_matrix.resize(9);
+   my_local_matrix.resize(3);
 
-   /*for(auto i = Laplacian_Stencil.begin(); i < Laplacian_Stencil.end(); ++i)
-       for(auto j = i->begin(); j < i->end(); ++j)
-        my_local_matrix*/
-   for(size_t i = 0; i < 3; ++i){
-        for(size_t j = 0; j < 3; ++j){
-
-            real a =Helmholtz_Stencil[i][j];
-            real b = Laplacian_Stencil[i][j];
-            my_local_matrix[i][j]= a - b;
-}
-        cout<<endl;
-}
-    /*for(auto iter = my_local_matrix.begin(); iter < my_local_matrix.end(); ++iter)
-            cout << my_local_matrix[1] <<endl;
-   cout<< my_local_matrix[1][1] <<endl;
+   for(auto i = my_local_matrix.begin(); i < my_local_matrix.end(); ++i)
+       i->resize(3);
 
    for(size_t i = 0; i < 3; ++i)
    {
         for(size_t j = 0; j < 3; ++j)
-        cout<< my_local_matrix[i][j] << '\t';
-   cout<<endl;
-   }*/
+        {
+        my_local_matrix[i][j] = Laplacian_Stencil[i][j] - Helmholtz_Stencil[i][j];
+        }
+   }
 
-cout<<my_local_matrix[1][1]<<endl;
+   cout<< "Local stiffness Matrix" << endl;
+   for(auto i = my_local_matrix.begin(); i < my_local_matrix.end(); ++i)
+   {
+       for(auto j = i->begin(); j< i->end() ; ++j)
+           cout<< *j << '\t';
+   cout<<endl;
+   }
+
+   face_num++;
+
 }
 
 void Matrix_Generator::generate()
 {
-    cout<< "I am in generate" << endl;
     i.process();
     int count =0;
         for(auto iter=i.F.begin();iter<i.F.end();++iter)
